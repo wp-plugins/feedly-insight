ce110cf3b7839d0d5e413e2c8ccbe9b853d4dba2
hayashikejinan <hayashikejinan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-30 16:22:25 +0000

    0.3.2 bump
    git-svn-id: https://plugins.svn.wordpress.org/feedly-insight/trunk@848368 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feedly-insight.php
+++ feedly-insight.php
@@ -4,7 +4,7 @@
 Plugin Name: Feedly Insight
 Plugin URI: http://wordpress.org/plugins/feedly-insight/
 Description: Add Feedly dashboard widget. Shows your site info, search website & feeds.
-Version: 0.3.1
+Version: 0.3.2
 Author: hayashikejinan
 Author URI: http://hayashikejinan.com/
 Text Domain: feedly_insight
