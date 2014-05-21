7b945e146471781c91ab7d73919ba2275b31f331
hayashikejinan <hayashikejinan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-21 14:17:45 +0000

    Bug fix ( Fatal error Jetpack
    git-svn-id: https://plugins.svn.wordpress.org/feedly-insight/trunk@918715 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- feedly-insight.php
+++ feedly-insight.php
@@ -61,7 +61,7 @@ class FI {
 		self::$option = get_option( FI_OPTION_NAME );
 		load_plugin_textdomain( FI_TEXT_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 		add_action( 'admin_init', array( $this, '_set_plugin_data' ) );
-		add_action( 'wp_loaded', array( $this, 'jetpack' ) );
+		add_action( 'plugins_loaded', array( $this, 'jetpack' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_css' ) );
 		if ( self::$option['css_enqueue'] )
 			add_action( 'wp_enqueue_scripts', array( $this, 'fi_button_css' ) );
@@ -89,7 +89,9 @@ class FI {
 	}
 
 	function jetpack() {
-		require_once dirname( __FILE__ ) . '/inc/jetpack/class_Jetpack.php';
+		if ( class_exists( 'Jetpack' ) ) {
+			require_once dirname( __FILE__ ) . '/inc/jetpack/class_Jetpack.php';
+		}
 	}
 
 	/**
---
+++ inc/jetpack/class_Jetpack_Share.php
@@ -0,0 +1,124 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+/**
+ * Class Share_Feedly
+ *
+ * for share daddy
+ */
+class Share_Feedly extends Sharing_Source {
+
+	var $shortname = 'feedly';
+
+	public function __construct( $id, array $settings ) {
+		parent::__construct( $id, $settings );
+
+		if ( 'official' == $this->button_style )
+			$this->smart = true;
+		else
+			$this->smart = false;
+	}
+
+	public function get_name() {
+		return __( 'Feedly', 'feedly_insight' );
+	}
+
+	public function get_display( $post ) {
+		if ( $this->smart ):
+			return '<div class="feedly_button">' . fi_get_button() . '</div>';
+		else:
+			$share_count = '';
+			if ( fi_get_subscribers() )
+				$share_count = '<span class="share-count">' . fi_get_subscribers() . '</span>';
+			return $this->get_link(
+				'http://cloud.feedly.com/#subscription%2F' . rawurlencode( 'feed/' . FI::$option['feed_url'] ),
+				_x( 'Feedly', 'follow us', 'feedly_insight' ) . $share_count,
+				__( 'Click to follow on Feedly', 'feedly_insight' ) );
+		endif;
+	}
+
+	public function display_footer() {
+		$this->js_dialog( $this->shortname, array( 'height' => 600, 'width' => 968, ) );
+	}
+
+}
+
+
+/**
+ * Class Share_Hatena
+ */
+class Share_Hatena extends Sharing_Source {
+
+	var $shortname = 'hatena';
+
+	public function __construct( $id, array $settings ) {
+		parent::__construct( $id, $settings );
+
+		if ( 'official' == $this->button_style )
+			$this->smart = true;
+		else
+			$this->smart = false;
+	}
+
+	public function get_share_url( $post_id ) {
+		return apply_filters( 'sharing_permalink', get_permalink( $post_id ), $post_id, $this->id );
+	}
+
+	public function get_share_title( $post_id ) {
+		$post  = get_post( $post_id );
+		$title = apply_filters( 'sharing_title', $post->post_title, $post_id, $this->id );
+
+		return html_entity_decode( wp_kses( $title, null ) );
+	}
+
+	public function get_name() {
+		return __( 'Bookmark', 'feedly_insight' );
+	}
+
+	public function get_display( $post ) {
+
+		$share_url  = esc_url( $this->get_share_url( $post->ID ) );
+		$post_title = esc_attr( $this->get_share_title( $post->ID ) );
+		// replace white space
+		$post_title = str_replace( ' ', '%20', $post_title );
+
+		if ( $this->smart ):
+			$lang = 'en';
+			$lang = apply_filters( 'sharing_hatena_lang', $lang );
+
+			$button = sprintf( '<a href="http://b.hatena.ne.jp/entry/" class="hatena-bookmark-button" data-hatena-bookmark-layout="standard-balloon" data-hatena-bookmark-lang="%s" data-hatena-bookmark-url="%s"  data-hatena-bookmark-title="%s" title="%s">%s</a>',
+				$lang, $share_url, $post_title, __( 'Add this entry to Hatena Bookmark', 'feedly_insight' ),
+				__( 'Add this entry to Hatena Bookmark', 'feedly_insight' ),
+				'<img src="http://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="' . __( 'Add this entry to Hatena Bookmark', 'feedly_insight' ) . '" width="20" height="20" style="border: none;" />'
+			);
+			return '<div class="hatena_button">' . $button . '</div>';
+		else:
+			return $this->get_link(
+				'http://b.hatena.ne.jp/entry/panel/?url=' . $share_url . '&amp;btitle=' . $post_title,
+				_x( 'Bookmark', 'share to', 'feedly_insight' ),
+				__( 'Click to share on Hatena Bookmark', 'feedly_insight' ),
+				'share=hatena', 'sharing-hatena-' . $post->ID );
+		endif;
+	}
+
+	public function display_footer() {
+		if ( $this->smart ) {
+			echo '<script type="text/javascript" src="//api.b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>';
+		} else {
+			$this->js_dialog( $this->shortname, array( 'height' => 220, 'width' => 365, ) );
+		}
+
+		if ( $this->button_style != 'icon' )
+			$this->hatena_enqueue();
+	}
+
+	public function hatena_enqueue() {
+		wp_enqueue_script( 'hatena-sharing-js', FI_URL . 'js/hatena-sharing.min.js', array( 'sharing-js' ), FI_VER );
+	}
+
+}
+
